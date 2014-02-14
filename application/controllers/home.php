f56e635b7fe28816d4b2ad755a797370e17da1ad
MrMM1989 <mrmm@live.nl>
2014-02-14 01:27:15 +0100

    Default Controller Veranderd
    De default 'Welcome' Controller veranderd naar een default 'Home'
    Controller

---
+++ application/controllers/home.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+/*
+	 *  Home controller - index method - Homepage site
+	 * 
+	 * 	Maps to: 	- domain.com
+	 * 				- domain.com/index.php
+	 *  			- domain.com/index.php/home
+	 * 				- domain.com/index.php/home/index	
+	*/
+	public function index()
+	{
+		
+	}
+}
+
+/* End of file home.php */
+/* Location: ./application/controllers/home.php */
