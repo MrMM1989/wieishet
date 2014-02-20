f3723e68d03ce539c7d8964cc9e7bd708865c830
MrMM1989 <mrmm@live.nl>
2014-02-20 15:00:01 +0100

    Basis pagina structuur versie 2
    Basis structuur van een pagina (versie 2)

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -2,17 +2,35 @@
 
 class Home extends CI_Controller {
 
-/*
+	/**
 	 *  Home controller - index method - Homepage site
 	 * 
 	 * 	Maps to: 	- domain.com
+	 * 				- domain.com/home
+	 * 				- domain.com/home/index
 	 * 				- domain.com/index.php
 	 *  			- domain.com/index.php/home
 	 * 				- domain.com/index.php/home/index	
 	*/
 	public function index()
 	{
-		$this->load->view('header.php');
+		$this->_load_views('game/play.php', 'Speel');
+	}
+	
+	/**
+	 * Loads the views
+	 * 
+	 * @param string $view - the view needed to be loaded in the page body
+	 * @param string $title - the page title
+	 */ 
+	private function _load_views($view, $title)
+	{
+		$data = array (
+			'title'=> $title
+		);
+		
+		$this->load->view('header.php', $data);
+		$this->load->view($view);
 		$this->load->view('footer.php');
 	}
 }
--- application/views/header.php
+++ application/views/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title><?php //echo $title; ?>|Wie is het?</title>
+    <title><?php echo $title; ?>|Wie is het?</title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>public/css/reset.css"  />
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>public/css/base.css"  />
   </head>
