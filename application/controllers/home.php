91be4ce9be421aa179be66c80e43f24f4cfd49a1
MrMM1989 <mrmm@live.nl>
2014-02-20 14:25:19 +0100

    Basis pagina structuur versie 1
    Basis structuur van een pagina

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -12,7 +12,8 @@ class Home extends CI_Controller {
 	*/
 	public function index()
 	{
-		
+		$this->load->view('header.php');
+		$this->load->view('footer.php');
 	}
 }
 
---
+++ application/views/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title><?php //echo $title; ?>|Wie is het?</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>public/css/reset.css"  />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>public/css/base.css"  />
+  </head>
+  <body>
+  
+  	
