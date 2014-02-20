df3826447e9b77637637a6662ebb8cd099c0ab6b
MrMM1989 <mrmm@live.nl>
2014-02-20 19:44:21 +0100

    Init database + install.txt versie2
    Een init SQL script voor initiële waarden in de database en de
    installatiehandleiding bijgewerkt.

---
+++ init_wie_is_het.sql
@@ -0,0 +1,33 @@
+-- -----------------------------------------------------
+-- INSERT `category`  initial values
+-- -----------------------------------------------------
+
+INSERT INTO category
+VALUES
+(NULL, 'Persoon'),
+(NULL, 'Voorwerp'),
+(NULL, 'Dier');
+
+
+-- -----------------------------------------------------
+-- INSERT `question`  initial values
+-- -----------------------------------------------------
+INSERT INTO question
+VALUES
+(NULL, 'Is het een persoon?', TRUE, 1),
+(NULL, 'Is het een man?', FALSE, 1),
+(NULL, 'Is het een vrouw?', FALSE, 1),
+(NULL, 'Draagt hij/zij een bril?', FALSE, 1),
+(NULL, 'Draagt hij/zij een hoofddeksel?', FALSE, 1),
+(NULL, 'Heeft hij/zij bruine ogen?', FALSE, 1),
+(NULL, 'Heeft hij/zij blauwe ogen?', FALSE, 1),
+(NULL, 'Heeft hij/zij groene ogen?', FALSE, 1),
+(NULL, 'Is hij/zij kaal?', FALSE, 1),
+(NULL, 'Heeft hij/zij zwart haar?', FALSE, 1),
+(NULL, 'Heeft hij/zij bruin haar?', FALSE, 1),
+(NULL, 'Heeft hij/zij blond haar?', FALSE, 1),
+(NULL, 'Heeft hij/zij rood haar?', FALSE, 1),
+(NULL, 'Heeft hij/zij grijs haar?', FALSE, 1),
+(NULL, 'Heeft hij/zij een voorwerp vast?', FALSE, 1),
+
+
